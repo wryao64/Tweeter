763a81b7d83eba22a67a70ee7fc8bbc2a2880386
Wendy <wryao64@gmail.com>
2019-06-03 22:06:39 +1200

    created base client server

---
+++ static/api/client_server.py
@@ -0,0 +1,43 @@
+
+
+def broadcast(username, password):
+    """
+    Transmits a signed broadcast between users
+    """
+    username = "wyao332"  # FOR TESTING PURPOSES
+    password = "wryao64_106379276"  # FOR TESTING PURPOSES
+
+def private_message(username, password):
+    """
+    Transmits a secret message between users.
+    """
+    username = "wyao332"  # FOR TESTING PURPOSES
+    password = "wryao64_106379276"  # FOR TESTING PURPOSES
+
+def check_messages(username, password):
+    """
+    Retrieve already-sent messages from other clients in the network
+    """
+    username = "wyao332"  # FOR TESTING PURPOSES
+    password = "wryao64_106379276"  # FOR TESTING PURPOSES
+
+def ping_check(username, password):
+    """
+    Checks is another client is active
+    """
+    username = "wyao332"  # FOR TESTING PURPOSES
+    password = "wryao64_106379276"  # FOR TESTING PURPOSES
+
+def group_message(username, password):
+    """
+    Transmits a secret group message between users
+    """
+    username = "wyao332"  # FOR TESTING PURPOSES
+    password = "wryao64_106379276"  # FOR TESTING PURPOSES
+
+def group_invite(username, password):
+    """
+    Transmits a secret group message between users
+    """
+    username = "wyao332"  # FOR TESTING PURPOSES
+    password = "wryao64_106379276"  # FOR TESTING PURPOSES
